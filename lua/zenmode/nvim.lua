a80203d6f9b18c0dcb98181057c703e81c381efc
I0I-I0I <azudjndj@gmail.com>
2025-07-03 04:41:54 +0300

    feat: toggle opts

--- lua/zenmode/nvim.lua
+++ lua/zenmode/nvim.lua
@@ -1,8 +1,9 @@
 ---@class Opts
 ---@field default_width number
----@field toggle_vals table | nil
+---@field toggle_opts table | nil
 
 ---@class Windows
+---@field M integer
 ---@field H integer
 ---@field L integer
 
@@ -10,6 +11,8 @@ local M = {}
 
 ---@type Windows[]
 local tabs = {}
+
+local saved_opts = nil
 
 ---@type Opts
 local opts = {
@@ -25,7 +28,7 @@ function M.setup(user_opts)
     end
 
     opts.default_width = user_opts.default_width or opts.default_width
-    opts.toggle_vals = user_opts.toggle_vals or opts.toggle_vals
+    opts.toggle_opts = user_opts.toggle_opts or opts.toggle_opts
 
     vim.api.nvim_create_user_command("ZenmodeToggle", function(input)
         M.zenmode_toggle(tonumber(input.fargs[1]))
@@ -52,9 +55,12 @@ function M.zenmode_open(input_width)
     local start_tab = vim.api.nvim_get_current_tabpage()
     local editor_tabs = vim.api.nvim_list_tabpages()
 
+    saved_opts = utils.save_opts(opts.toggle_opts)
+
     for _, current_tab in pairs(editor_tabs) do
         vim.api.nvim_set_current_tabpage(current_tab)
         tabs[current_tab] = utils.zenmode_open_one(input_width)
+        utils.apply_opts(opts.toggle_opts)
     end
 
     vim.api.nvim_set_current_tabpage(start_tab)
@@ -65,9 +71,10 @@ function M.zenmode_close()
         return
     end
 
-    for tab, _ in pairs(tabs) do
-        utils.zenmode_close_one(tabs[tab])
-        tabs[tab] = nil
+    for current_tab, _ in pairs(tabs) do
+        utils.zenmode_close_one(tabs[current_tab])
+        utils.apply_opts(saved_opts)
+        tabs[current_tab] = nil
     end
 end
 
